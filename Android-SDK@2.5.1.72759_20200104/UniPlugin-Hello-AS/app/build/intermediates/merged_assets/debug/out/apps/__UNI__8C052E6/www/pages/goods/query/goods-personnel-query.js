de6ea41a04de37a5ea2be6c3d1721dbaf637beb0
武新义 <ly-080208@163.com>
2020-02-20 18:12:32 +0800

    第一次上传

---
+++ Android-SDK@2.5.1.72759_20200104/UniPlugin-Hello-AS/app/build/intermediates/merged_assets/debug/out/apps/__UNI__8C052E6/www/pages/goods/query/goods-personnel-query.js
@@ -0,0 +1,1 @@
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/goods/query/goods-personnel-query.wxml') } }));
---
+++ Android-SDK@2.5.1.72759_20200104/UniPlugin-Hello-AS/dth-rp-al/src/main/java/com/yotexs/dth/rp/service/DthRpService.java
@@ -0,0 +1,35 @@
+package com.yotexs.dth.rp.service;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.security.rp.RPSDK;
+import com.taobao.weex.WXSDKEngine;
+import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.bridge.JSCallback;
+
+/**
+ * 人脸服务
+ */
+public class DthRpService extends WXSDKEngine.DestroyableModule {
+
+    public String verifyToken = "verifyToken";
+
+    @JSMethod(uiThread = true)
+    public  void show(JSONObject options, final JSCallback jsCallback){
+        String content = options.getString(verifyToken);
+        RPSDK.start(verifyToken, mWXSDKInstance.getContext(), new RPSDK.RPCompletedListener(){
+            @Override
+            public void onAuditResult(RPSDK.AUDIT audit, String s) {
+                JSONObject result = new JSONObject();
+                result.put("audit", audit);
+                result.put("s", s);
+                jsCallback.invoke(result);
+            }
+        });
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
